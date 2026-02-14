0d05ca4c1a3565695246f0f76772ac5bf0833943
Gareth Pile <pile.gareth@gmail.com>
2026-02-14 21:09:34 +0200

    Add CORS headers to API Lambda responses

--- amplify/functions/tradingApi/handler.ts
+++ amplify/functions/tradingApi/handler.ts
@@ -106,6 +106,9 @@ const json = (statusCode: number, payload: unknown): APIGatewayProxyResult => ({
   statusCode,
   headers: {
     'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Headers': 'Content-Type,Authorization',
+    'Access-Control-Allow-Methods': 'GET,POST,OPTIONS',
   },
   body: JSON.stringify(payload),
 });
